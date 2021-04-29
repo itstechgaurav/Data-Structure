210fa1eb910e1d2eedd25a964b4dcaf2e37e0dba
gaurav bhardwaj <myselfgroot@gmail.com>
2021-04-29 15:57:19 +0530

    @added [01 let;s code recursion.c -> recursion]

---
+++ Recursion/01 let's code recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void func(int n)
+{
+    if (n > 0)
+    {
+        printf("%d\n", n);
+        func(n - 1); // calling the recursive function again
+    }
+}
+
+int main()
+{
+    int x = 3;
+    func(x);
+    return 0;
+}
